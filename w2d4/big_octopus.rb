6244fd5c5428cd3a1a4fb8a230d0f42a989be9c8
Adom Hartell <adomhartell@gmail.com>
2016-11-25 12:59:55 -0800

    implement dominant_octopus and clever_octopus

--- w2d4/big_octopus.rb
+++ w2d4/big_octopus.rb
@@ -1,5 +1,48 @@
+# search for the largest string using different algorithms
+
+# quadradic search
 def sluggish_octopus(array)
   array.each do |el1|
     return el1 if array.all? { |el2| el1.length >= el2.length }
+  end
+end
+
+# n log n search
+def dominant_octopus(array)
+  length_sort = proc { |el1, el2| el1.length <=> el2.length }
+  sorted_array = array.merge_sort(&length_sort)
+  sorted_array.last
+end
+
+# constant search
+def clever_octopus(array)
+  largest_string = ""
+  array.each do |string|
+    largest_string = string if string.length > largest_string.length
+  end
+
+  largest_string
+end
+
+class Array
+  def merge_sort(&prc)
+    return self if size <= 1
+    prc ||= proc { |el1, el2| el1 <=> el2 }
+    mid_point = self.size / 2
+    left_sorted = self.take(mid_point).merge_sort(&prc)
+    right_sorted = self.drop(mid_point).merge_sort(&prc)
+    Array.merge(left_sorted, right_sorted, &prc)
+  end
+
+  def self.merge(left, right, &prc)
+    merged = []
+    until left.empty? || right.empty?
+      case prc.call(left.first, right.first)
+      when -1 then merged << left.shift
+      else merged << right.shift
+      end
+    end
+
+    merged + left + right
   end
 end
