a7bb4c1bb979cade55b9429ee3472a7ab5a87aba
Adom Hartell <adomhartell@gmail.com>
2016-11-30 12:32:25 -0800

    complete rails DemoProject

--- w3d2/DemoProject/db/schema.rb
+++ w3d2/DemoProject/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161130202147) do
+ActiveRecord::Schema.define(version: 20161130202904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
