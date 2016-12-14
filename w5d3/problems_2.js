1c92f8d4ffb304df98bae64e1a9d039ed411245f
Adom Hartell <adomhartell@gmail.com>
2016-12-14 06:27:23 -0800

    add elephant contructor and methods

--- w5d3/problems_2.js
+++ w5d3/problems_2.js
@@ -8,3 +8,29 @@ function titleize(names, callback) {
 titleize(["Mary", "Brian", "Leo"], (names) => {
   names.forEach(name => console.log(name));
 });
+
+
+// Phase II - Constructors, Prototypes, and `this`
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  trickIndex = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+};
