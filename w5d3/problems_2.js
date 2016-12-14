ab3bff3feae08179e34a66607ae5b6e3306b603a
Adom Hartell <adomhartell@gmail.com>
2016-12-14 06:35:32 -0800

    phase IV - closures

--- w5d3/problems_2.js
+++ w5d3/problems_2.js
@@ -50,3 +50,15 @@ Elephant.paradeHelper = function (elephant) {
 };
 
 herd.forEach(Elephant.paradeHelper);
+
+// Phase IV - Closures
+
+function dinerBreakfast() {
+  let order = "I'd like cheesy scrambled eggs please.";
+  console.log(order);
+
+  return function (food) {
+    order = `${order.slice(0, order.length - 8)} and ${food} please.`;
+    console.log(order);
+  };
+};
