9359302294c3681e320655860e4d18914197ee87
Adom Hartell <adomhartell@gmail.com>
2016-12-21 13:21:43 -0800

    add handler for new places

--- w6d4/script.js
+++ w6d4/script.js
@@ -20,6 +20,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
+  const addFavorite = (e) => {
+    e.preventDefault();
+
+    const placeInputEl = document.querySelector(".favorite-input");
+    const placeName = placeInputEl.value;
+    placeInputEl.value = "";
+
+    const placesList = document.getElementById("sf-places");
+    const newPlace = document.createElement("li");
+    newPlace.textContent = placeName;
+
+    placesList.appendChild(newPlace);
+
+  };
+
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", addFavorite);
+
 
 
   // adding new photos
