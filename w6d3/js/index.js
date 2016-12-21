a8f68c89001c73c4a31612f22db3c82e527c5234
Adom Hartell <adomhartell@gmail.com>
2016-12-21 06:30:03 -0800

    log weather data on successful request

--- w6d3/js/index.js
+++ w6d3/js/index.js
@@ -5,7 +5,7 @@ $.ajax( {
   type: 'GET',
   url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
   success(data) {
-    console.log("cool cool");
+    console.log(data);
   },
 });
 // Add another console log here, outside your AJAX request
