e10f59ee61059b4bc21f84fad2468d7755a0247a
Adom Hartell <adomhartell@gmail.com>
2017-01-04 13:08:05 -0800

    add presentaitional container component

--- w7d3/components/giphys_search.jsx
+++ w7d3/components/giphys_search.jsx
@@ -1,3 +1,43 @@
 import React, { Component } from 'react';
 
 import GiphysIndex from './giphys_index';
+
+class GiphysSearch extends Component {
+
+  constructor() {
+    super();
+    this.state = { query: "birthday party" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchSearchGiphys("birthday+party");
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.currentTarget.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let query = this.state.query.split(" ").join("+");
+    this.props.fetchSearchGiphys(query);
+  }
+
+  render() {
+    let { giphys } = this.props;
+
+    return (
+      <div>
+        <form className='search-bar'>
+          <input value={this.state.query} onChange={this.handleChange} />
+          <button type="submit" onClick={this.handleSubmit}>Search</button>
+        </form>
+        <GiphysIndex giphys={giphys} />
+      </div>
+    );
+  }
+}
+
+export default GiphysSearch;
